dc178ea6b47464f020d830e43f55951ae67bcf85
pyaephyokyaw10499-hash <pyaephyokyaw104.99@gmail.com>
2026-05-27 21:32:50 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -116,13 +116,13 @@ install_x-ui() {
     cd /usr/local/
 
     if [ $# == 0 ]; then
-        last_version=$(curl -Ls "https://api.github.com/repos/pyaephyokyaw10499-hsah/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        last_version=$(curl -Ls "https://api.github.com/repos/pyaephyokyaw10499-hsah/x-ui-pyaephyokyaw/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$v1.10.2" ]]; then
             echo -e "${red}Failed to fetch x-ui version, it maybe due to Github API restrictions, please try it later${plain}"
             exit 1
         fi
         echo -e "Got x-ui latest version: ${last_version}, beginning the installation..."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/pyaephyokyaw10499-hsah/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/pyaephyokyaw10499-hsah/x-ui-pyaephyokyaw/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}Dowanloading x-ui failed, please be sure that your server can access Github ${plain}"
             exit 1
