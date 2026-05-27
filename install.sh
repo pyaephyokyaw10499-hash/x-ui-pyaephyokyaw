37e9c02853c7e103399fac24edc3c608857f0bf0
pyaephyokyaw10499-hash <pyaephyokyaw104.99@gmail.com>
2026-05-27 21:48:56 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -129,7 +129,7 @@ install_x-ui() {
         fi
     else
         last_version=$1
-        url="https://github.com/alireza0/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
+        url="https://github.com/pyaephyokyaw10499-hsah/x-ui-pyaephyokyaw/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
         echo -e "Begining to install x-ui v$1"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
         if [[ $? -ne 0 ]]; then
